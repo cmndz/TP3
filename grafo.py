6f3316c7fce631b8019b1919f78a11afd415ec9f
Carlos <c.al.m.13@hotmail.com>
2019-07-03 22:03:44 -0300

    Create grafo.py

---
+++ grafo.py
@@ -0,0 +1,71 @@
+class Grafo:
+    def __init__(self):
+        '''Crea un grafo vacio'''
+        self._vertices = {}
+        self._cantidadVertices = 0
+    def verticeAgregar(self, vertice):
+        '''Agrega un vertice'''
+        if not vertice in self._vertices.keys():
+            self._vertices[vertice] = {}
+            self._cantidadVertices += 1
+            return True
+        return False
+    def verticeRemover(self, vertice):
+        '''Elimina un vertice'''
+        if self.estaVacio() or not vertice in self._vertices.keys():return None
+        for w in self._vertices.keys(): 
+            if vertice in self._vertices[w].keys(): self._vertices[w].pop(vertice)
+        self._cantidadVertices -= 1
+        return self._vertices.pop(vertice)
+    def verticeCantidad(self):
+        '''Devuelve la cantidad de vertices que contiene el grafo'''
+        return self._cantidadVertices
+    def aristaAgregar(self, verticeOrigen, verticeDestino):
+        '''Agrega una arista'''
+        if self.estaVacio() or not verticeOrigen in self._vertices.keys() or not verticeDestino in self._vertices.keys():return False
+        self._vertices[verticeOrigen][verticeDestino] = ''
+        return True
+    def aristaRemover(self, verticeOrigen, verticeDestino):
+        '''Elimina una arista'''
+        if self.estaVacio() or not verticeOrigen in self._vertices.keys() or not verticeDestino in self._vertices.keys():return None
+        return self._vertices[verticeOrigen].pop(verticeDestino)
+    def estaVacio(self):
+        '''Devuelve true si esta vacio, sino false'''
+        return self._cantidadVertices == 0
+    
+    def vertices(self):
+        '''Devuelve una lista con los vertices'''
+        return self._vertices.keys()
+    def adyacentes(self, v):
+        '''Devuelve una lista con los adyacentes'''
+        return self._vertices[v].keys()
+    def estanConectados(self, a, b):
+        '''Devuelve true si estan conectados, sino false'''
+        if not a in self._vertices.keys() or not b in self._vertices.keys():return False
+        return a in self._vertices[b].keys() and b in self._vertices[a].keys()
+    def __str__(self):
+        cadena = ""
+        for v in self._vertices.keys():
+            cadena += v + ":"
+            for w in self._vertices[v].keys():
+                cadena += w + " "
+            cadena += "\n"
+        return cadena
+
+g = Grafo()
+print(g)
+print('Esta vacio?', g.estaVacio())
+print('Agrego A',g.verticeAgregar('A'))
+print('Agrego B',g.verticeAgregar('B'))
+print('Agrego C',g.verticeAgregar('C'))
+print('Esta vacio?', g.estaVacio())
+print('Cantidad?', g.verticeCantidad())
+print('Agrego AC',g.aristaAgregar('A','C'))
+print('Agrego CB',g.aristaAgregar('C','B'))
+print('Agrego AB',g.aristaAgregar('A','B'))
+print(g)
+
+#g.verticeRemover('A')
+#g.verticeRemover('B')
+print(g.vertices())
+#print(g.)
