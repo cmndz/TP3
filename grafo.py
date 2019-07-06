1bb9b2e099d24107cea75be010d179b4682805ac
Carlos <c.al.m.13@hotmail.com>
2019-07-06 18:48:31 -0300

    Update grafo.py

--- grafo.py
+++ grafo.py
@@ -1,55 +1,76 @@
+import random
+
 class Grafo:
-    def __init__(self):
-        '''Crea un grafo vacio'''
-        self._vertices = {}
-        self._cantidadVertices = 0
-    def verticeAgregar(self, vertice):
-        '''Agrega un vertice'''
-        if not vertice in self._vertices.keys():
-            self._vertices[vertice] = {}
-            self._cantidadVertices += 1
-            return True
-        return False
-    def verticeRemover(self, vertice):
-        '''Elimina un vertice'''
-        if self.estaVacio() or not vertice in self._vertices.keys():return None
-        for w in self._vertices.keys(): 
-            if vertice in self._vertices[w].keys(): self._vertices[w].pop(vertice)
-        self._cantidadVertices -= 1
-        return self._vertices.pop(vertice)
-    def verticeCantidad(self):
-        '''Devuelve la cantidad de vertices que contiene el grafo'''
-        return self._cantidadVertices
-    def aristaAgregar(self, verticeOrigen, verticeDestino):
-        '''Agrega una arista'''
-        if self.estaVacio() or not verticeOrigen in self._vertices.keys() or not verticeDestino in self._vertices.keys():return False
-        self._vertices[verticeOrigen][verticeDestino] = ''
-        return True
-    def aristaRemover(self, verticeOrigen, verticeDestino):
-        '''Elimina una arista'''
-        if self.estaVacio() or not verticeOrigen in self._vertices.keys() or not verticeDestino in self._vertices.keys():return None
-        return self._vertices[verticeOrigen].pop(verticeDestino)
-    def estaVacio(self):
-        '''Devuelve true si esta vacio, sino false'''
-        return self._cantidadVertices == 0
-    
-    def vertices(self):
-        '''Devuelve una lista con los vertices'''
-        return self._vertices.keys()
-    def adyacentes(self, v):
-        '''Devuelve una lista con los adyacentes'''
-        return self._vertices[v].keys()
-    # def estanConectados(self, a, b):
-    #     '''Devuelve true si estan conectados, sino false'''
-    #     if not a in self._vertices.keys() or not b in self._vertices.keys():return False
-    #     return a in self._vertices[b].keys() and b in self._vertices[a].keys()
-    def __str__(self):
-        cadena = ""
-        for v in self._vertices.keys():
-            cadena += v + ":"
-            for w in self._vertices[v].keys():
-                cadena += w + " "
-            cadena += "\n"
-        return cadena
+	"""Representación del TAD Grafo"""
+
+	def __init__(self):
+		"""Crea una grafo vacío."""
+		self.grafo = {}
+
+
+	def agregar_vertice(self, vertice):
+		"""Agrega un vertice al grafo."""
+		self.grafo[vertice] = {}
+
+
+	def borrar_vertice(self, vertice):
+		"""Borra un vertice del grafo."""
+		for clave in self.grafo:
+
+			if vertice in self.grafo[clave]:
+				self.grafo[clave].pop(vertice)				
+
+		if vertice not in self.grafo:
+			return None
+
+		return self.grafo.pop(vertice)
+
+
+	def agregar_arista(self, vertice_1, vertice_2, peso = 0):
+		"""Agrega una arista entre dos vertices."""
+		self.grafo[vertice_1][vertice_2] = peso
+		
+
+	def borrar_arista(self, vertice_1, vertice_2):
+		"""Borra una arista entre dos vertices."""
+
+		if vertice_2 not in self.grafo[vertice_1]:
+			return None
+
+		return self.grafo[vertice_1].pop(vertice_2)
+
+	def vertices_conectados(self, vertice_1, vertice_2):
+		"""Devuelve si dos vertices estan conectados."""
+		return vertice_2 in self.grafo[vertice_1]
+
+
+	def vertice_random(self):
+		"""Devuelve un vertice al azar del grafo."""
+		return random.choice(list(self.grafo.keys()))
+
+
+	def vertices(self):
+		"""Obtiene todos los vertices del grafo."""
+		return self.grafo.keys()
+
+
+	def adyacentes(self, vertice):
+		"""Obtiene todos los adyacentes a un vertice."""
+		return self.grafo[vertice].keys()
+
+
+	def __iter__(self):
+		"""Devuelve un iterador sobre los vertices."""
+		return iter(self.grafo)
+
+
+	def __str__(self):
+		cadena = ""
+		for v in self.grafo.keys():
+			cadena += v + ": "
+			for w in self.grafo[v].keys():
+				cadena += w + " "
+			cadena += "\n"
+		return cadena
 
 
