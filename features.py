883be663e121a6e9763e343a93fddf8043f87039
Carlos <c.al.m.13@hotmail.com>
2019-07-06 18:48:37 -0300

    Update features.py

--- features.py
+++ features.py
@@ -1,40 +1,44 @@
 from grafo import * 
+from collections import deque
 
 #-------------------------------------------------------------------#
 #                           AUXILIARES                              #
 #-------------------------------------------------------------------#
 def caminoMinimo(grafo, origen):
-    visitados = {}
-    distancia = {}
+    visitados = set()
     padre = {}
-    q = []
+    distancia = {}
+    queue = deque()
     #-----
-    for v in grafo.vertices():distancia[v] = None
+    for v in grafo.vertices():
+        distancia[v] = None
+        padre[v] = None
     #-----
-    visitados[origen] = True
     distancia[origen] = 0
-    padre[origen] = None
-    q.append(origen)
+    queue.append(origen)
     #-----
-    while q:
-        v = q.pop(0)
+    while queue:
+        v = queue.popleft()
+        visitados.add(v)
         for w in grafo.adyacentes(v):
-            if not w in visitados.keys():
-                visitados[w] = True
-                distancia[w] = distancia[v]+1
+            if not w in visitados:
                 padre[w] = v
-                q.append(w)
+                distancia[w] = distancia[v]+1
+                queue.append(w)
     return distancia, padre
+
+#def ordenar_vertices(grafo, distancias):
+
 
 def calcularCentralidad( grafo ):
     centralidad = {}
     for v in grafo.vertices(): centralidad[v] = 0
     for v in grafo.vertices():
-        distancia, padre = caminoMinimo(grafo,v)
-        
-        
+        #distancia, padre = caminoMinimo(grafo,v)
         aux = {}
         for w in grafo.vertices():aux[w] = 0
+        #vertices_ordenados = ordenar_vertices(grafo, distancias)
+        
         
 
     return centralidad
@@ -45,25 +49,25 @@ def calcularCentralidad( grafo ):
 #-------------------------------------------------------------------#
 def min_seguimientos(grafo, origen, destino):
     '''Imprime el Camino Minimo entre un Vertice de Origen y uno de Destino, si es que fueran conexos.'''
-    if not origen in grafo.vertices() or destino not in grafo.vertices():
-        print("Seguimiento Imposible")
+    if not origen in grafo.vertices() or destino not in grafo.vertices():   #Nos aseguramos que ambos 
+        print("Seguimiento Imposible")                                      #vertices se encuetren en el Grafo.
         return
-    
-    distancia, padre = caminoMinimo(grafo, origen)
-    if not distancia[destino]:
-        print("Seguimiento Imposible")
-        return
+    _, padre = caminoMinimo(grafo, origen)
+    if not padre[destino]:                                                  #Nos aseguramos de que ambos 
+        print("Seguimiento Imposible")                                      #vertices esten en la misma 
+        return                                                              #componente conexa.
+    stack = []
+    v = destino
+    while v:                                                                #Apilamos los vertices,
+        stack.append(v)                                                     #desde el Destino, 
+        v = padre[v]                                                        #hacia el Origen.
 
-    lista = []
-    lista.append(destino)
-    v = padre[destino]
-    while v:
-        lista.append(v)
-        v = padre[v]
-    lista.reverse()
-    print(lista[0], end = "")
-    for v in lista[1:]:print(" -> ", v , sep = "", end = "")
-    print()
+    while stack:                                                            #Desapilamos, mientras
+        print( stack.pop() , end = "" )                                     #imprimimos el seguimiento.
+        if stack:
+            print(" -> ", end = "")
+            continue
+        print()
     return
 
 def mas_imp(grafo, cant):
@@ -78,30 +82,29 @@ def mas_imp(grafo, cant):
 
 g = Grafo()
 print(g)
-print("Esta vacio?", g.estaVacio())
-print("Agrego A",g.verticeAgregar('A'))
-print("Agrego B",g.verticeAgregar('B'))
-print("Agrego C",g.verticeAgregar('C'))
-print("Agrego D",g.verticeAgregar('D'))
-print("Agrego E",g.verticeAgregar('E'))
-print("Agrego F",g.verticeAgregar('F'))
-print("Esta vacio?", g.estaVacio())
-print("Cantidad?", g.verticeCantidad())
-print("Agrego AB",g.aristaAgregar('A','B'))
-print("Agrego BC",g.aristaAgregar('B','C'))
-print("Agrego CD",g.aristaAgregar('C','D'))
-print("Agrego AE",g.aristaAgregar('A','E'))
-print("Agrego BF",g.aristaAgregar('B','F'))
-print("Agrego DF",g.aristaAgregar('D','F'))
-print("Agrego DF",g.aristaAgregar('D','F'))
-print("Agrego DF",g.aristaAgregar('D','F'))
+
+print("Agrego A",g.agregar_vertice('A'))
+print("Agrego B",g.agregar_vertice('B'))
+print("Agrego C",g.agregar_vertice('C'))
+print("Agrego D",g.agregar_vertice('D'))
+print("Agrego E",g.agregar_vertice('E'))
+print("Agrego F",g.agregar_vertice('F'))
+
+print("Agrego AB",g.agregar_arista('A','B'))
+print("Agrego BC",g.agregar_arista('B','C'))
+print("Agrego CD",g.agregar_arista('C','D'))
+print("Agrego AE",g.agregar_arista('A','E'))
+print("Agrego BF",g.agregar_arista('B','F'))
+print("Agrego DF",g.agregar_arista('D','F'))
+print("Agrego DF",g.agregar_arista('D','F'))
+print("Agrego DF",g.agregar_arista('D','F'))
 print(g)
 #print('Adyacentes de A',g.adyacentes('A'))
 min_seguimientos(g, 'A', 'F')
-print("Remuevo DF",g.aristaRemover('B','F'))
+print("Remuevo DF",g.borrar_arista('B','F'))
 print(g)
 min_seguimientos(g, 'A', 'F')
-print("Remuevo B", g.verticeRemover('B'))
+print("Remuevo B", g.borrar_vertice('B'))
 min_seguimientos(g, 'A', 'F')
 #g.verticeRemover('A')
 #g.verticeRemover('B')
