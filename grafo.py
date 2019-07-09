52f4c107e388c010c4eaef542f542c916ee3c4d0
Carlos <c.al.m.13@hotmail.com>
2019-07-09 17:58:47 -0300

    Update grafo.py

--- grafo.py
+++ grafo.py
@@ -1,25 +1,25 @@
 import random
 
 class Grafo:
-	"""Representación del TAD Grafo"""
+	'''Representación del TAD Grafo.'''
 
 	def __init__(self):
-		"""Crea una grafo vacío."""
+		'''Crea una grafo vacío.'''
 		self.grafo = {}
 		self.cantidad = 0
 
 	def cantidad_vertices(self):
-		"""Devuelve la cantidad de vertices en ell grafo."""
+		'''Devuelve la cantidad de vertices en el Grafo.'''
 		return self.cantidad
 
 	def agregar_vertice(self, vertice):
-		"""Agrega un vertice al grafo."""
+		'''Agrega un vertice al Grafo, si es que este no existe dentro.'''
+		if vertice in self.grafo.keys():return
 		self.grafo[vertice] = {}
 		self.cantidad += 1
 
-
 	def borrar_vertice(self, vertice):
-		"""Borra un vertice del grafo."""
+		'''Elimina y devuelve un vertice del Grafo, si es que existe dentro.'''
 		if vertice not in self.grafo:
 			return None
 
@@ -31,46 +31,42 @@ class Grafo:
 		self.cantidad -= 1
 		return self.grafo.pop(vertice)
 
-
 	def agregar_arista(self, vertice_1, vertice_2, peso = 0):
-		"""Agrega una arista entre dos vertices."""
+		'''Agrega una arista entre dos vertices.'''
+		if (vertice_2 in self.grafo[vertice_1].keys() and 
+			self.grafo[vertice_1][vertice_2] == peso):
+			return
 		self.grafo[vertice_1][vertice_2] = peso
-		
 
 	def borrar_arista(self, vertice_1, vertice_2):
-		"""Borra una arista entre dos vertices."""
-
+		'''Borra una arista entre dos vertices.'''
 		if vertice_2 not in self.grafo[vertice_1]:
 			return None
 
 		return self.grafo[vertice_1].pop(vertice_2)
 
 	def vertices_conectados(self, vertice_1, vertice_2):
-		"""Devuelve si dos vertices estan conectados."""
+		'''Devuelve si dos vertices estan conectados.'''
 		return vertice_2 in self.grafo[vertice_1]
 
-
 	def vertice_random(self):
-		"""Devuelve un vertice al azar del grafo."""
+		'''Devuelve un vertice al azar del grafo.'''
 		return random.choice(list(self.grafo.keys()))
 
-
 	def vertices(self):
-		"""Obtiene todos los vertices del grafo."""
+		'''Obtiene todos los vertices del grafo.'''
 		return self.grafo.keys()
 
-
 	def adyacentes(self, vertice):
-		"""Obtiene todos los adyacentes a un vertice."""
+		'''Obtiene todos los adyacentes a un vertice.'''
 		return self.grafo[vertice].keys()
 
-
 	def __iter__(self):
-		"""Devuelve un iterador sobre los vertices."""
+		'''Devuelve un iterador sobre los vertices.'''
 		return iter(self.grafo)
 
-
 	def __str__(self):
+		'''Devuelve una cadena con el Grafo como Lista de Adyacencias.'''
 		cadena = ""
 		for v in self.grafo.keys():
 			cadena += v + ": "
